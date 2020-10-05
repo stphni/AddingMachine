735148c0bba6207d4aa106f74b14e822e3950e9b
Stephanie M <Stephanie M@LAPTOP-GBJOFCTA>
2020-10-04 19:34:49 -0700

    Initial commit of project.

---
+++ CSE360Assignment01/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,63 @@
+/**
+* The AddingMachine program implements methods that will
+* add, subtract, get the total, display the arithmetic
+* and clear the data.
+* @author  Stephanie Mejia Vazquez
+*/
+package cse360assignment02;
+
+public class AddingMachine {
+	
+	 private int total;
+	  
+	  /**
+	   * This is the constructor of the AddingMachine class.
+	   * @param args unused.
+	   * @return none.
+	  */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * This method return the total.
+	   * @param args unused.
+	   * @return int. This returns the total sum. 
+	  */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * This method adds the parameter value to the total variable.
+	   * @param args value. This parameter is an integer.
+	   * @return void.
+	  */
+	  public void add (int value) {
+	  }
+	  
+	  /**
+	   * This method subtracts the parameter value from the total variable.
+	   * @param args value. This parameter is an integer.
+	   * @return void.
+	  */
+	  public void subtract (int value) {
+	  }
+	  
+	  /**
+	   * This method displays the history transaction as a string.
+	   * @param args unused.
+	   * @return String. This returns the history transaction. It starts from the initial 0 value to the last input as a string. 
+	  */
+	  public String toString () {
+	    return "";
+	  }
+	  
+	  /**
+	   * This method clears the data.
+	   * @param args unused.
+	   * @return void.
+	  */
+	  public void clear() {
+	  }
+}
