266030eae17ba4515ea90ad8b56f8e2df6c5129a
Stephanie M <Stephanie M@LAPTOP-GBJOFCTA>
2020-10-04 21:26:36 -0700

    Implemented functionalities to methods.

--- CSE360Assignment01/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment01/src/cse360assignment02/AddingMachine.java
@@ -9,6 +9,7 @@ package cse360assignment02;
 public class AddingMachine {
 	
 	 private int total;
+	 private String record;
 	  
 	  /**
 	   * This is the constructor of the AddingMachine class.
@@ -17,6 +18,7 @@ public class AddingMachine {
 	  */
 	  public AddingMachine () {
 	    total = 0;  // not needed - included for clarity
+	    record = "" + total; // record is a string (keeps track of history transaction) set to start string at 0. 
 	  }
 	  
 	  /**
@@ -25,7 +27,7 @@ public class AddingMachine {
 	   * @return int. This returns the total sum. 
 	  */
 	  public int getTotal () {
-	    return 0;
+	    return total; // return the total
 	  }
 	  
 	  /**
@@ -34,6 +36,8 @@ public class AddingMachine {
 	   * @return void.
 	  */
 	  public void add (int value) {
+		  total = total + value; // total is equal to the total plus the parameter value
+		  record +=  " + " + value; // append the parameter value to record string
 	  }
 	  
 	  /**
@@ -42,6 +46,8 @@ public class AddingMachine {
 	   * @return void.
 	  */
 	  public void subtract (int value) {
+		  total = total - value; // total is equal to the total minus the parameter value
+		  record += " - " + value; // append the parameter value to record string
 	  }
 	  
 	  /**
@@ -50,7 +56,7 @@ public class AddingMachine {
 	   * @return String. This returns the history transaction. It starts from the initial 0 value to the last input as a string. 
 	  */
 	  public String toString () {
-	    return "";
+	    return record; // return the entire record String that appended all the values
 	  }
 	  
 	  /**
@@ -59,5 +65,7 @@ public class AddingMachine {
 	   * @return void.
 	  */
 	  public void clear() {
+		  total = 0; // set the total to be cleared, set to 0
+		  record = "" + total; // set record string to hold no data
 	  }
 }
